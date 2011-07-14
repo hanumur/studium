632d9e00799864b019559c3f2800ed93997d212d
Arturo Herrero <arturo.herrero@gmail.com>
2011-07-15 00:38:15 +0200

    Exercises 1.16 - 1.16

--- chapter1/ex1.13.scm
+++ chapter1/ex1.13.scm
@@ -2,10 +2,11 @@
 !#
 
 ;; Exercise 1.13: Prove that Fib(n) is the closest integer to Φ**n/√5, where Φ = (1 + √5)/2.
-;; Hint: Let Φ = (1 - √5)/2. Use induction and the definition of the Fibonacci numbers
-;; (see section 1.2.2) to prove that Fib(n) = (Φ**n - Φ**n)/√5.
+;; Hint: Let ψ = (1 - √5)/2. Use induction and the definition of the Fibonacci numbers
+;; (see section 1.2.2) to prove that Fib(n) = (Φ**n - ψ**n)/√5.
 
 
-; Huu? I don't remember anything about mathematical induction.
-; A good explanation of this exercise can be found at: http://wiki.drewhess.com/wiki/SICP_exercise_1.13
+; Huu? I don't remember anything about mathematical induction. I promise come back!
+; A good explanation of this exercise can be found at:
+; http://plagelao.github.com/SICP/2011/07/section-1-2-2/
 
