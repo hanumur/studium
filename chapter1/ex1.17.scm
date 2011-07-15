5262e9b161c1bc3fd04b1508b552f300ef77a154
Arturo Herrero <arturo.herrero@gmail.com>
2011-07-15 20:14:45 +0200

    Scheme already provides the primitive predicate even?

--- chapter1/ex1.17.scm
+++ chapter1/ex1.17.scm
@@ -24,9 +24,6 @@
 (define (halve a)
     (/ a 2))
 
-(define (even? n)
-  (= (remainder n 2) 0))
-
 (define (fast-mult a b)
     (cond ((= b 0) 0)
         ((even? b) (double (fast-mult a (halve b))))
