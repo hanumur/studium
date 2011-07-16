58f0154ca4c8db167c56e258d1c5adba06999f0b
Arturo Herrero <arturo.herrero@gmail.com>
2011-07-16 15:09:49 +0200

    Fixed indentation, ex1.17

--- chapter1/ex1.17.scm
+++ chapter1/ex1.17.scm
@@ -26,6 +26,6 @@
 
 (define (fast-mult a b)
     (cond ((= b 0) 0)
-        ((even? b) (double (fast-mult a (halve b))))
-        (else (+ a (fast-mult a (- b 1))))))
+          ((even? b) (double (fast-mult a (halve b))))
+          (else (+ a (fast-mult a (- b 1))))))
 
