d45393eacb5628a69dc9c85fa38e3251773fe013
Arturo Herrero <arturo.herrero@gmail.com>
2011-11-16 00:42:01 +0100

    Added some blank lines like visual cues

--- 1-ruby/day-1/day-1.rb
+++ 1-ruby/day-1/day-1.rb
@@ -2,17 +2,22 @@
 # Print the string “Hello, world.”
 puts 'Hello, word.'
 
+
 # For the string “Hello, Ruby,” find the index of the word “Ruby.”
 'Hello, Ruby.'.index 'Ruby'
+
 
 # Print your name ten times.
 10.times { puts 'Arturo' }
 
+
 # Print the string “This is sentence number 1,” where the number 1 changes from 1 to 10.
 1.upto(10) { |num| puts "This is sentence number #{num}" }
 
+
 # Run a Ruby program from a file.
-    #ruby day-1.rb
+    # ruby day-1.rb
+
 
 # Write a program that picks a random number. Let a player guess the number,
 # telling the player whether the guess is too low or too high.
