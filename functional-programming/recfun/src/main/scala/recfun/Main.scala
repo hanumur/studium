28786b317c49a4838cb47add60bc8ffdfdae6d64
Arturo Herrero <arturo.herrero@gmail.com>
2013-04-07 13:08:30 +0100

    Recursion

--- functional-programming/recfun/src/main/scala/recfun/Main.scala
+++ functional-programming/recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,28 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean =
+    balance(chars, 0) == 0
+
+  def balance(chars: List[Char], parenthesesBalance: Int): Int =
+    if (chars.isEmpty) parenthesesBalance
+    else if (chars.head == ')' && parenthesesBalance == 0) return -1
+    else if (chars.head == ')') balance(chars.tail, parenthesesBalance - 1)
+    else if (chars.head == '(') balance(chars.tail, parenthesesBalance + 1)
+    else balance(chars.tail, parenthesesBalance)
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
 }
