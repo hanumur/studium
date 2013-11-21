7542d0ff1ba5628de8dc1e157b297079254716ba
Arturo Herrero <arturo.herrero@gmail.com>
2013-11-21 19:13:25 +0000

    Oh man! Trailing whitespaces

--- interview/combined-enumerator/combined_enumerator_test.rb
+++ interview/combined-enumerator/combined_enumerator_test.rb
@@ -1,6 +1,6 @@
-# The aim of this class is to take a number of ordered enumerators and then 
+# The aim of this class is to take a number of ordered enumerators and then
 # emit their values in ascending order.
-# 
+#
 # Some assumptions:
 #   * The enumerators passed in emit their values in ascending order.
 #   * The enumerators emit values which are Comparable[1] with each other.
@@ -48,44 +48,44 @@ if $0 == __FILE__
       enumerator = CombinedOrderedEnumerator.new([].to_enum, (1..10).to_enum)
       assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], enumerator.take(20)
     end
-    
+
     def test_enumerating_with_one_empty_array_and_finite_sequence_with_switched_args
       enumerator = CombinedOrderedEnumerator.new((1..10).to_enum, [].to_enum)
       assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], enumerator.take(20)
     end
-    
+
     def test_enumerating_an_infinite_sequence_and_finite_one
       enumerator = CombinedOrderedEnumerator.new(fibonacci, (1..10).to_enum)
       assert_equal [0, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 8, 8, 9, 10, 13, 21, 34], enumerator.take(20)
     end
-    
+
     def test_enumerating_two_infinite_sequences
       enumerator = CombinedOrderedEnumerator.new(fibonacci, sum_of_natural_numbers)
       assert_equal [0, 1, 1, 1, 2, 3, 3, 5, 6, 8, 10, 13, 15, 21, 21, 28, 34, 36, 45, 55], enumerator.take(20)
     end
-    
+
     def test_enumerating_three_finite_sequences
       enumerator = CombinedOrderedEnumerator.new((1..5).to_enum, (1..3).to_enum, (4..10).to_enum)
       assert_equal [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 9, 10], enumerator.take(20)
     end
-    
+
     def test_enumerating_three_infinite_sequences
       enumerator = CombinedOrderedEnumerator.new(fibonacci, fibonacci, fibonacci)
       assert_equal [0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2], enumerator.take(12)
     end
-    
+
     def test_raises_unordered_enumerator_exception_if_enumerator_isnt_in_ascending_order
       enumerator = CombinedOrderedEnumerator.new(10.downto(1))
-    
+
       assert_raises(CombinedOrderedEnumerator::UnorderedEnumerator) do
         enumerator.take(20)
       end
     end
-    
+
     def test_raising_unordered_enumerator_should_reference_enumerator
       decending_enumerator = 10.downto(1)
       enumerator = CombinedOrderedEnumerator.new(decending_enumerator)
-    
+
       begin
         enumerator.take(2)
         assert false
@@ -107,7 +107,7 @@ if $0 == __FILE__
           end
         end
       end
-    end      
+    end
 
     def fibonacci
       FibonacciEnumerator.new
